b64d45b98001a9b3d051397c5e98b69842128b31
saskia-dwi-ulfah <sdwiulfah@gmail.com>
2022-11-18 19:34:59 +0700

    DONE 2

--- healthyin/lib/screens/Artikel Page/healthyin_artikel_detail_page.dart
+++ healthyin/lib/screens/Artikel Page/healthyin_artikel_detail_page.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: prefer_typing_uninitialized_variables
-import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:webview_flutter/webview_flutter.dart';
